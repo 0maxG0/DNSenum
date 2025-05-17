b695295e545a84a28acc1f8e8c27e33ad219009c
0maxG0 <maxbusinessac@hotmail.com>
2025-05-17 22:07:06 +0900

    fix errors

--- detailed_subdomainEnum.py
+++ detailed_subdomainEnum.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#required Amass and httpx
+# required Amass and httpx
 
 import json
 import os
@@ -9,6 +9,7 @@ data = json.loads(json_data)
 
 target_domain = data["target_domain"]
 
-#create active/accecible domain lists
-os.system(f"amass enum -d {target_domain} -active | grep -v aaaa_record | grep -v ASN | grep -v : | cut -d' ' -f1 | sed -E 's/::.*$//; s/\/[0-9]+$/ > cleaned.txt")
+# create active/accessible domain lists
+os.system(
+    f"amass enum -d {target_domain} -active | grep -v aaaa_record | grep -v ASN | grep -v : | cut -d' ' -f1 | sed -E 's/::.*$//; s/\/[0-9]+$/ > cleaned.txt")
 os.system("httpx -l cleaned.txt -status-code -title -tech-detect -o httpx_results.txt")
