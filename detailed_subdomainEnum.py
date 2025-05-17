0a56a5c60aae4fe073f3bce0d0df6ec98660b0fa
0maxG0 <maxbusinessac@hotmail.com>
2025-05-17 22:05:23 +0900

    create enum scripts for creating subdomains list

---
+++ detailed_subdomainEnum.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#required Amass and httpx
+
+import json
+import os
+
+json_data = '{"target_domain":"google.com"}'
+data = json.loads(json_data)
+
+target_domain = data["target_domain"]
+
+#create active/accecible domain lists
+os.system(f"amass enum -d {target_domain} -active | grep -v aaaa_record | grep -v ASN | grep -v : | cut -d' ' -f1 | sed -E 's/::.*$//; s/\/[0-9]+$/ > cleaned.txt")
+os.system("httpx -l cleaned.txt -status-code -title -tech-detect -o httpx_results.txt")
